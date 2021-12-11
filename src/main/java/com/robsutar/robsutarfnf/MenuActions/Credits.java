63815d142f2527ac42c7526bdff75e4908cb7161
Robsutar <robsonsutar@gmail.com>
2021-12-11 11:11:18 -0300

    New Project

--- src/main/java/com/robsutar/robsutarfnf/MenuActions/Credits.java
+++
@@ -1,8 +0,0 @@
-package com.robsutar.robsutarfnf.MenuActions;
-
-public class Credits extends MenuAction{
-    @Override
-    protected void onAction() {
-        System.out.println("");
-    }
-}
--- src/main/java/com/robsutar/robsutarfnf/MenuActions/MenuAction.java
+++
@@ -1,8 +0,0 @@
-package com.robsutar.robsutarfnf.MenuActions;
-
-public abstract class MenuAction {
-    public void action(){
-        onAction();
-    }
-    protected abstract void onAction();
-}
